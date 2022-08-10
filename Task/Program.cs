a84cf3d73d03f040fab3509b6e07451f6060c0ce
Elena <gudulina@mail.ru>
2022-08-10 23:48:00 +0700

    Начало работы над решением задачи

---
+++ Task/Program.cs
@@ -0,0 +1,5 @@
+﻿/* Написать программу, которая из массива строк
+формирует массив строк, длина которых меньше либо равна 3 символа. */
+
+string[] arrayFirst = new string[4] {"hello", "2", "world", ":-)"};
+string[] arraySecond = new string[arrayFirst.Lenght];
