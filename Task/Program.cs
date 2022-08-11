b6b79850576f00234c0dddf6fa5000b71a9057e5
Elena <gudulina@mail.ru>
2022-08-11 20:42:27 +0700

    Функция со сравнением элементов

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,4 +2,18 @@
 формирует массив строк, длина которых меньше либо равна 3 символа. */
 
 string[] arrayFirst = new string[4] {"hello", "2", "world", ":-)"};
-string[] arraySecond = new string[arrayFirst.Lenght];
+string[] arraySecond = new string[arrayFirst.Length];
+
+void ArrayFormation (string[] arrayFirst, string[] arraySecond)
+{
+    int j = 0;
+    for (int i = 0; i < arrayFirst.Length; i++)
+    {
+        if (arrayFirst[i].Length <=3)
+        {
+            arraySecond[j] = arrayFirst[i];
+            j++;
+        }
+    }
+}
+
